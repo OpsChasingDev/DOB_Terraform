27e1d2bc2507dba6d0e6670141b99f0d8849e55a
Robert Stapleton <robert@stapleton-family.net>
2023-01-14 18:27:50 -0500

    added public/private key pair info for aws account

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,14 @@
+variable "aws_access_key" {
+  description = "access key for AWS account"
+}
+variable "aws_secret_key" {
+  description = "secret key for AWS account"
+}
+
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 variable "vpc_cidr_block" {}
